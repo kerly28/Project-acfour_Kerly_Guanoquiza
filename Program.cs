6e558c5a916801760728285e09b6d98f76c07a21
kerly28 <kerly.guanoquiza.7e8@itb.cat>
2024-11-25 12:57:47 +0100

    Closes #5 - Codi Csharp

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿//PRE: EL programa demana introduïr 4 números naturals
-
 using System;
-
 namespace ExamenSorpresa
 {
     public class Program
@@ -11,27 +9,24 @@ namespace ExamenSorpresa
         {
             return num > 0;
         }
-
         public static void Main()
         {
             //inici constants
             const string Msg = "Introdueix un número natural: ";
             //array per emmagatzemar els números
             int[] numbers = new int[4];
-
             //inici programa
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine(Msg);
                 numbers[i] = Convert.ToInt32(Console.ReadLine());
-
                 //Crideam al métode que comprova si el número és natural
                 if (!Natural(numbers[i]))
                 {
                     Console.WriteLine("Error: Has d'introduir un número natural.");
                     break;
                 }
-                else if (i==3)
+                else if (i == 3)
                 {
                     Console.WriteLine("Els números introduïts són: ");
                     for (int j = 0; j < 4; j++)
@@ -39,9 +34,8 @@ namespace ExamenSorpresa
                         Console.WriteLine(numbers[j]);
                     }
                 }
+            }
 
-            }   
-            
         }
     }
 }
